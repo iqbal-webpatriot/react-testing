d75ed0e174f2ac4181e46b41f02143be035dcf0c
iqbal.webpatriot <iqbal.webpatriot@gmail.com>
2023-08-01 11:22:13 +0530

    add eslint-prettier plugin

--- src/hooks/useCounter.test.tsx
+++ src/hooks/useCounter.test.tsx
@@ -1,11 +1,12 @@
-import { renderHook, act } from "@testing-library/react";
-import useCounter from "./useCounter";
-describe("useCounter hook", () => {
-  test("should render the initial count 0", () => {
+import { renderHook, act } from '@testing-library/react';
+import useCounter from './useCounter';
+describe('useCounter hook', () => {
+  test('should render the initial count 0', () => {
     const { result } = renderHook(useCounter);
     expect(result.current.count).toBe(0);
   });
-  test("should accept and render same initial count", () => {
+
+  test('should accept and render same initial count', () => {
     const { result } = renderHook(useCounter, {
       initialProps: {
         initialValue: 10,
@@ -13,12 +14,12 @@ describe("useCounter hook", () => {
     });
     expect(result.current.count).toBe(10);
   });
-  test("should increment the initial count by 1", () => {
+  test('should increment the initial count by 1', () => {
     const { result } = renderHook(useCounter);
     act(() => result.current.incrementCountHandler());
     expect(result.current.count).toBe(1);
   });
-  test("should decrement the initial count by -1", () => {
+  test('should decrement the initial count by -1', () => {
     const { result } = renderHook(useCounter);
     act(() => result.current.decrementCountHandler());
     expect(result.current.count).toBeGreaterThanOrEqual(0);
