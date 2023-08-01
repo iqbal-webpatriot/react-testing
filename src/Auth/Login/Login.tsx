fa6477189fc53d22ff5ed7b11b9b00e84c5f3af9
iqbal.webpatriot <iqbal.webpatriot@gmail.com>
2023-08-01 16:03:24 +0530

    add form input handling

--- src/Auth/Login/Login.tsx
+++ src/Auth/Login/Login.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { ErrorMessage, LoginInputSate } from './login.types';
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [userInput, setUserInput] = useState<LoginInputSate>(
+    {} as LoginInputSate,
+  );
+  const [errorMessage, setErrorMessage] = useState<ErrorMessage>(
+    {} as ErrorMessage,
+  );
 
   const handleLogin = async (event: React.SyntheticEvent) => {
     event.preventDefault();
@@ -11,41 +15,49 @@ export default function Login() {
     try {
       const response = await fetch('https://api.example.com/login', {
         method: 'post',
-        body: JSON.stringify({
-          username,
-          password,
-        }),
+        body: JSON.stringify(userInput),
       });
       const jsonResult = await response.json();
       // Successful login, now redirect or do whatever you want with the response
       console.log('Success:', jsonResult);
     } catch (error) {
       // Handle error and set error message
-      setError('Invalid username or password');
     }
   };
+  const inputHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setUserInput({ ...userInput, [name]: value });
+    setErrorMessage({});
+  };
+  console.log('user input', userInput);
   return (
     <>
       <div className="App">
         <form onSubmit={handleLogin}>
           <label htmlFor="username">Username:</label>
+          <br />
           <input
             id="username"
             type="text"
             name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={userInput.username}
+            onChange={inputHandler}
           />
+          <br />
           <label htmlFor="password">Password:</label>
+          <br />
           <input
             id="password"
             type="password"
             name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={userInput.password}
+            onChange={inputHandler}
           />
+          <br />
           <button type="submit">Login</button>
-          {error && <div role="alert">{error}</div>}
+          {errorMessage?.invalidLogin && (
+            <div role="alert">{errorMessage?.invalidLogin}</div>
+          )}
         </form>
       </div>
     </>
--- src/Auth/Login/login.types.ts
+++ src/Auth/Login/login.types.ts
@@ -1,4 +1,7 @@
-export type LoginSate = {
+export type LoginInputSate = {
   username: string;
   password: string;
+};
+export type ErrorMessage = {
+  [key: string]: string;
 };
